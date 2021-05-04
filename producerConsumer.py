903b76d4dfde8c460ea0eb3d5612668d5a21fde4
emmanuel <ealvarez19@miners.utep.edu>
2021-05-03 23:02:53 -0600

    Video Player Working

--- producerConsumer.py
+++ producerConsumer.py
@@ -5,28 +5,28 @@ lock = threading.Lock()
 class producerConsumerQueue():
 
     def __init__(self):
-        self.full = threading.Semaphore(0)
-        self.empty = threading.Semaphore(10)
+        self.full = threading.Semaphore(0)    #0 spots available
+        self.empty = threading.Semaphore(10)  #10 spots available
         self.framesQueue = queue.Queue()
 
     def insertFrame(self,frame):               #insert into queue
-        self.empty.acquire()
-        lock.acquire()
-        self.framesQueue.put(frame)
+        self.empty.acquire()                   #decrement empty (take available spot away)
+        lock.acquire()                         #avoid collisions
+        self.framesQueue.put(frame)            #insert frame
         lock.release()
-        self.full.release()
+        self.full.release()                    #increment full (take a spot)
         
 
-    def getFrame(self):                         #get and delete from queue
-        self.full.acquire()
+    def getFrame(self):                        #get and delete from queue
+        self.full.acquire()                    #decrement full (remove spot full)
         lock.acquire()
-        frame = self.framesQueue.get()
+        frame = self.framesQueue.get()         #remove/get frame
         lock.release()
-        self.empty.release()
-        return Frame
+        self.empty.release()                   #increment empty (another empty spot available)
+        return frame
 
-    def isEmpty(self):                         #check if is empty
+    def isEmpty(self):                         #check if queue is empty
         lock.acquire()
-        isEmpty = framesQueue.empty()
+        isEmpty = self.framesQueue.empty()
         lock.release()
         return isEmpty
--- videoPlayer.py
+++ videoPlayer.py
@@ -3,9 +3,8 @@ import queue
 from producerConsumer import producerConsumerQueue
 import os
 import cv2
-
+import numpy as np
 clipFile = "clip.mp4"
-
 ########################################################
 def extractFrames(clipFile,colorQueue):
     count = 0                                            #count frames
@@ -13,41 +12,58 @@ def extractFrames(clipFile,colorQueue):
 
     success,image = vidcap.read()                        #image bitmap
 
-    while success and count < 72:                        
-        success,jpegImage = cv2.imencode('.jpeg',image)  #convert image to jpeg
-        colorQueue.insertFrame(jpegImage)                #insert to colorQueue
+    while success and count < 72:
+        print("Extracting frame ",count)
+        colorQueue.insertFrame(image)                    #insert to colorQueue
         success,image = vidcap.read()                    #get next image
         count+=1
-
     print("Extraction complete")
+    colorQueue.insertFrame(None)                         #mark the end of the queue
 
 ########################################################
 def convertFrames(colorQueue,grayQueue):
+    count = 0
     while True:
-        if colorQueue.empty():                           #if there are not frames continue
+        if colorQueue.isEmpty():                           #if there are not frames continue
+            #print("Color queue empty")
             continue
-        colorFrame = colorQueue.getFrame()
-        grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        grayQueue.insertFrame(grayFrame)
+        else:            
+            colorFrame = colorQueue.getFrame()             #get colorFrame
+            if colorFrame is None:                         #if it is the end of the colorqueue, end
+                break
+            print("Converting frame %i to gray scale" % (count))
+            grayFrame = cv2.cvtColor(colorFrame,cv2.COLOR_BGR2GRAY)
+            grayQueue.insertFrame(grayFrame)               #insert grayFrame to grayQueue
+            count+=1
+    print("Finished converting frames")
+    grayQueue.insertFrame(None)                            #mark the end of the grayqueue
 #########################################################          
 def displayFrames(grayQueue):
+    count = 0
     while True:
-        if grayFrame.isEmpty():                         #if there are not gray frames
+        if grayQueue.isEmpty():                            #if there are not gray frames
             continue
-        if count == 72:                                 
-            break
-        displayFrame = grayFrame.getFrame()             #frame to display
-        cv2.imshow('Video',frame)                       #show frame to display
-        if cv2.waitKey(42) and 0xFF == ord('q'):        #wait 42 msec
-            break
-        count+=1                  
+        else:
+            grayFrame = grayQueue.getFrame()               #get frame to display
+            if grayFrame is None:                          #if it is the end of grayqueue, end
+                break
+            print("Displaying Frame %i" % (count))
+            cv2.imshow('Video',grayFrame)                  #show frame to display
+            if cv2.waitKey(42) and 0xFF == ord("q"):       #wait 42 msec
+                break
+            count+=1                  
     cv2.destroyAllWindows()                  
-    
+    print("Finished displaying all frames")
 #########################################################    
 
-colorQueue = producerConsumerQueue()
-grayQueue = producerConsumerQueue()
+colorQueue = producerConsumerQueue()                        #queue with color frames
+grayQueue = producerConsumerQueue()                         #queue with gray frames
 
 extractThread = threading.Thread(target = extractFrames, args = (clipFile,colorQueue))
 convertThread = threading.Thread(target = convertFrames, args = (colorQueue,grayQueue))
-displayThread = threading.Thread(target = convertFrames, args = (grayQueue))
+displayThread = threading.Thread(target = displayFrames, args = {grayQueue})
+
+#start threads
+extractThread.start()
+convertThread.start()
+displayThread.start()
